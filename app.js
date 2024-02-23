9cd1053657e2d9d2bddd9399016fc5b90b156369
Piuma Nera <moh.messafa@gmail.com>
2024-02-23 10:28:16 +0100

    my battery is low

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const port = 3000
-const Blog = require(`./models/blog`);
+const blogRoutes = require(`./routes/blogRoutes`)
 const dbUri = `mongodb+srv://hanoun00:hanoun1234@hisoka.yxifnzk.mongodb.net/dbtest?retryWrites=true&w=majority&appName=Hisoka`
              
 mongoose.connect(dbUri)
@@ -27,58 +27,9 @@ app.get(`/about`,(req,res)=>{
     res.render(`about`,{title:`About`});
 })
 
-app.get('/blogs', (req,res)=>{
-    Blog.find().sort({createdAt: -1})
-    .then((result)=>{res.render('index',{title:'All Blogs', blogs:result})})
-    .catch((err)=>{console.log(err)})
-});
+//Blog routes
 
-app.post('/blogs',(req,res)=>{
-    const blog = new Blog(req.body);
-    blog.save()
-    .then((result)=>{
-        res.redirect('/blogs');
-    }).catch((err)=>{
-        console.log(err);
-    })
-})
-
-
-app.delete( '/blogs/:id' , ( req , res )=>{
-    const id=req.params.id;
-    Blog.findByIdAndDelete({_id : id})
-    .then(()=>{
-        res.json({redirect: '/blogs'})
-    })
-    .catch((err)=> {
-        console.log(err)
-    });
-});
-
-
-
-app.get('/blogs/create',(req,res)=>{
-        res.render('create', {title:`Create a blog`})
-});
-
-app.get('/blogs/:id',(req,res)=>{
-    const id = req.params.id;
-    Blog.findById(id)
-    .then((result)=>{
-        res.render('details', {title:'Blog Details' , blog: result})
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-});
-
-
-app.get('/single-blog', (req,res)=>{
-    Blog.findById()
-    .then((result)=>{res.send(result)})
-    .catch((err)=>{console.log(err)})
-});
-
+app.use(`/blogs`,blogRoutes); //adds the blog route to our application. It's like saying "
 
 
 app.use(``,(req,res)=>{
